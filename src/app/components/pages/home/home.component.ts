1bec65e53ad6b7b43bc51e90fa6482ec19851aa7
Starlin de la cruz <starlinr82@gmail.com>
2020-01-24 12:53:35 -0400

    Showing all the posts and them tags

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -4,19 +4,14 @@ import { PostInterface } from '../../../shared/model/post.interface';
 import { Observable } from 'rxjs';
 
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  public posts: {
-    id: string;
-    titlePost: string;
-    contentPost: string;
-    imagePost: string
-  }[] = [  ];
 
   public posts$: Observable<PostInterface[]>;
 
